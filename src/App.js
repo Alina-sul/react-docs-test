e38bb54f6a284cff6f6b17503c9f11e10ff79442
Alina Sul <48648714+Alina-sul@users.noreply.github.com>
2020-07-03 20:31:59 +0300

    Code-Splitting
    Using React.lazy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Suspense} from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+const Home = React.lazy(() => import('./routes/Home'));
+const About = React.lazy(() => import('./routes/About'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/about" component={About}/>
+          </Switch>
+        </Suspense>
+      </Router>
+    </>
   );
 }
 
---
+++ src/routes/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About = () => (
+    <div>
+        This is about
+    </div>
+);
+
+export default About;
