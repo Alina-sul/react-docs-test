e2f8d8ae05eb3083a51e2414d030738b2e3ccd73
Alina Sul <48648714+Alina-sul@users.noreply.github.com>
2020-07-04 16:13:41 +0300

    # Refs #
    Dom ref vs Class ref tests

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -10,18 +10,34 @@ function Home(props) {
             <ThemeContext.Provider value="dark">
                 <Text />
                 <TextFunc />
+                <br/><br/>
+                <AutoFocus />
             </ThemeContext.Provider>
         </>
     );
 }
 
 class Text extends React.Component {
+    constructor(props){
+        super(props);
+        this.inputText = React.createRef();
+        this.focusInputText= this.focusInputText.bind(this);
+    }
+
     static contextType = ThemeContext;
+
+    focusInputText() {
+        this.inputText.current.focus();
+        console.log(this.inputText)
+    }
+
 
     render(){
         return (
             <>
-                <Button theme={this.context} name="Text btn" />
+                <input style={{width: 300}} type="text" ref={this.inputText}
+                       value={this.props.value ? this.props.value : `Ref for DOM - will focus after clicking on 'Text btn'`} /> <br />
+                <Button theme={this.context} name="Text btn" onClick={this.focusInputText} />
             </>
         )
     }
@@ -43,14 +59,35 @@ function TextFunc (){
 
 function Button(props) {
     const [count,setCount] = React.useState(0);
-    console.log(props.theme);
-    const error = () => {throw new Error('I crashed!')};
+    const error = () => {throw new Error('I crashed to test ErrorBoundary!!!')};
     return (
         <>
-            <button onClick={()=> setCount(count + 1)}> {props.name} </button>
+            <button onClick={()=> {
+                if(props.onClick) props.onClick();
+                setCount(count + 1)
+            }}> {props.name} </button>
             <span> {count < 2 ? count : error() } </span>
         </>
     );
+}
+
+class AutoFocus extends React.Component {
+    constructor(props){
+        super(props);
+        this.inputText = React.createRef();
+
+    }
+
+    componentDidMount() {
+        this.inputText.current.focusInputText();
+    }
+
+    render(){
+        console.log(this.props);
+        return (
+            <Text ref={this.inputText} value="Ref for class - auto focus"/>
+        )
+    }
 }
 
 export default Home;
